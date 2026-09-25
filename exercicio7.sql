f655036d75af5789aeffd4162b3c9e921372f392
Pedro-AntonioGBA <oliveira.viana@academico.ifpb.edu.br>
2026-09-25 00:41:23 -0300

    Update exercicio7.sql

--- exercicio7.sql
+++ exercicio7.sql
@@ -2,10 +2,10 @@ begin;
 set transaction isolation level Read committed;
 -- vi: Na primeira pesquise com o read committed a transação A não conseguiu ver a transação B
 select * from farmacia;
+-- viii: Quando a transação B confirma a consulta, a transação A consegue ver o resultado anda da transação A ar o commit
 commit;
--- viii: Quando a transação B confirma a consulta a transação A consegue ver o resultado
-select * from farmacia
 -- x: Ele apenas confirma o nivel de isolamento
+select * from farmacia
 rollback
 
 
